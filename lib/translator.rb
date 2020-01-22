aecaaa5daa9432e4da7d41e5df3670f3ca8ea6a6
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-22 22:51:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
+
+def load_library(file)
+  ehash = {:get_emoticon => {}, :get_meaning => {}}
+  YAML.load_file(file).each do  |meaning, array|
+  #  binding.pry
+    english, japanese = array
+    ehash["get_emoticon"][english] = japanese
+    ehash["get_meaning"][japanese] = meaning
+  end
+end
+
+def get_japanese_emoticon(emoticons, emoti)
+  ehash = load_library(emoticons)
+  ehash.each do |key, value|
+    ehash[:get_emoticon][key] = value
+    ehash[:get_meaning][key] = value
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+
+
+
+
+
 end
 
-def get_english_meaning
+def get_english_meaning(emoticons, emoti)
+  ehash = YAML.load_file(emoticons)
+  emoti.each do |key, value|
+    ehash[:get_emoticon][key] = value
+    ehash[:get_meaning][key] = value
   # code goes here
+end
 end
