a8946d7cca215fdfd667d827284252d777ecea20
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-22 22:52:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,38 +2,33 @@
 require "pry"
 require "yaml"
 
-
 def load_library(file)
   ehash = {:get_emoticon => {}, :get_meaning => {}}
   YAML.load_file(file).each do  |meaning, array|
-  #  binding.pry
     english, japanese = array
-    ehash["get_emoticon"][english] = japanese
-    ehash["get_meaning"][japanese] = meaning
+    ehash[:get_emoticon][english] = japanese
+    ehash[:get_meaning][japanese] = meaning
   end
+  ehash
 end
 
 def get_japanese_emoticon(emoticons, emoti)
   ehash = load_library(emoticons)
-  ehash.each do |key, value|
-    ehash[:get_emoticon][key] = value
-    ehash[:get_meaning][key] = value
-  # code goes here
+  if ehash[:get_emoticon][emoti]
+    return  ehash[:get_emoticon][emoti]
+  else
+    "Sorry, that emoticon was not found"
+  end
+ehash
 end
 
-
-
-
-
-
-
-end
 
 def get_english_meaning(emoticons, emoti)
-  ehash = YAML.load_file(emoticons)
-  emoti.each do |key, value|
-    ehash[:get_emoticon][key] = value
-    ehash[:get_meaning][key] = value
-  # code goes here
-end
-end
+  ehash = load_library(emoticons)
+  if ehash[:get_emoticon][emoti]
+    return  ehash[:get_emoticon][emoti]
+  else
+    "Sorry, that emoticon was not found"
+  end
+  ehash
+  end
